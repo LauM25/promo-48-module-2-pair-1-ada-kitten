3884257dfcfa5df98ac6601ce732fd32952a6803
Patrigar10 <patri.garcia.gomez@gmail.com>
2025-02-21 12:09:30 +0100

    Search

--- js/main.js
+++ js/main.js
@@ -53,5 +53,31 @@ btnCancel.addEventListener ("click", () => {
     newForm.classList.add("collapsed");
 })
 
+// 1- Definir lo que vamos a usar 
+// 2- Escuchar la acción click del botón
+      //- Almacenar el valor a buscar (input description)
+      //- Si el valor de descripción coincide con la descripción de los gatos
+
+
+
+const btnSearch = document.querySelector(".js_button-search");
+const description = document.querySelector(".js_in_search_desc");
+const KitterDesc1 = "Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.";
+const KitterDesc2 = "Produce fascinación y curiosidad. Exótico, raro, bello, extraño… hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo";
+const KitterDesc3 = "Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.";
+const catSection = document.querySelector(".js-list");
+
+btnSearch.addEventListener("click", (event) => {
+    event.preventDefault(btnSearch);
+    const inputDescription = description.value; ;
+    if (KitterDesc1.includes(inputDescription)) {
+        catSection.innerHTML = kittenOne;
+
+
+
+
+    }
+    
+  });
 
 
